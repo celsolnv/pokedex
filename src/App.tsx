268f8dc3a2e2433b06476eedc3a18c4d3a25fedd
Celso <celsolnv27@gmail.com>
2023-02-27 17:54:06 -0300

    feat: list of pokemons in main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Header } from './components/Header'
 import { getPokemonDetails, getPokemons } from './services/api'
 import { type IPokemon } from './services/api/interfaces'
 
@@ -21,16 +22,28 @@ export default function App (): JSX.Element {
   }
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Bem-vindo a Pokedex
-      </h1>
-      {pokemons.map(pokemon => (
-        <ul key={pokemon.number}>
-          <li>{pokemon.name}</li>
-          <li>{pokemon.baseExp}</li>
-          <li>{pokemon.number}</li>
-        </ul>
-      ))}
+      <Header/>
+      <div className="flex justify-start flex-wrap ">
+        {pokemons.map(pokemon => (
+          <div
+            key={pokemon.number}
+            className="
+              w-50 h-50 bg-white border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700
+              flex flex-col items-center m-1 p-5
+              "
+          >
+            <a href="#">
+                <img className="w-24 h-24 " src={pokemon.image} alt="" />
+            </a>
+            <div className="">
+                <p className=" font-normal text-gray-700 dark:text-gray-400">
+                  {pokemon.name}
+                </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
     </div>
   )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export function Header (): JSX.Element {
+  return (
+    <header className="bg-red-700 mx-auto flex items-center justify-center p-4 lg:px-8">
+      <img src='../../assets/images/logo-pokedex.png' alt="logo do pokemon" />
+    </header>
+  )
+}
